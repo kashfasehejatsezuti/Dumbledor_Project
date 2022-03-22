d89be58d42a0816f97b5b915e36f4f9d8d50a81c
Kashfa Sehejat Sezuti <75807451+kashfasehejatsezuti@users.noreply.github.com>
2022-03-22 21:48:01 +0100

    Update gryffindor.component.ts

--- src/app/Components/gryffindor/gryffindor.component.ts
+++ src/app/Components/gryffindor/gryffindor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { FormBuilder,FormGroup} from '@angular/forms';
 import { ApiService } from '../../shared/api.service';
 import { AlliesModel } from './gryffindor.model';
 
