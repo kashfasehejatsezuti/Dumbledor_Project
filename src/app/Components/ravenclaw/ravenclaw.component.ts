8a21c90fe6bdc8aba3145e413071ef7c775c1418
Kashfa Sehejat Sezuti <75807451+kashfasehejatsezuti@users.noreply.github.com>
2022-03-22 21:49:40 +0100

    Update ravenclaw.component.ts

--- src/app/Components/ravenclaw/ravenclaw.component.ts
+++ src/app/Components/ravenclaw/ravenclaw.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/api.service';
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { FormBuilder,FormGroup} from '@angular/forms';
 import { AlliesravModel } from './ravenclaw.model';
 
 @Component({
